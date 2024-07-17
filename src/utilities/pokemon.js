2b241a600c6f21fde9384770c9fb0863614a962c
David Antonio <happywybie@gmail.com>
2024-07-16 22:53:59 -0600

    feat: add display method to show API data as a test

--- src/utilities/pokemon.js
+++ src/utilities/pokemon.js
@@ -7,9 +7,9 @@ async function fetchData() {
   try {
     const response = await fetch(
       `https://pokeapi.co/api/v2/pokemon?limit=${max_pokemon}&offset=0`
-    ); 
+    );
 
-    const data = await response.json(); 
+    const data = await response.json();
     pokemons = data.results;
     console.log(pokemons);
     displayPokemons(pokemons);
@@ -20,4 +20,23 @@ async function fetchData() {
 
 fetchData();
 
+function displayPokemons(pokemon) {
+  listDisplay.innerHTML = "";
 
+  pokemon.forEach((pokemon) => {
+    const pokemonID = pokemon.url.split("/")[6];
+    const listItem = document.createElement("div");
+    listItem.className = "list-item";
+    listItem.innerHTML = `
+  
+            <div class="img-wrap">
+                <img src="https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/dream-world/${pokemonID}.svg" alt="${pokemon.name}" />
+            </div>
+            <div class="name-wrap">
+                <p class="body3-fonts">${pokemon.name}</p>
+            </div>
+        `;
+
+    listDisplay.appendChild(listItem);
+  });
+}
