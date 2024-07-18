e0b7cabd0ed648ad7f2a1b19dbcc7e01f0bbb874
David Antonio <happywybie@gmail.com>
2024-07-17 21:51:23 -0600

    ci: add style and function of modal

--- src/utilities/pokemon.js
+++ src/utilities/pokemon.js
@@ -1,4 +1,4 @@
-const max_pokemon = 20;
+const max_pokemon = 1;
 const listDisplay = document.querySelector(".list-display");
 const main = document.querySelector(".main");
 
@@ -13,7 +13,7 @@ async function fetchData() {
     const data = await response.json();
     pokemons = data.results;
     console.log(pokemons);
-    displayPokemons(pokemons);
+    // displayPokemons(pokemons);
   } catch (error) {
     console.error("error:", error);
   }
@@ -42,3 +42,29 @@ function displayPokemons(pokemon) {
     main.appendChild(card);
   });
 }
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
