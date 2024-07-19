77d93a327164e3d7c34b923ce72375319bf49100
David Antonio <happywybie@gmail.com>
2024-07-18 20:20:46 -0600

    test: modal on card

--- src/utilities/pokemon.js
+++ src/utilities/pokemon.js
@@ -13,7 +13,7 @@ async function fetchData() {
     const data = await response.json();
     pokemons = data.results;
     console.log(pokemons);
-    // displayPokemons(pokemons);
+    displayPokemons(pokemons);
   } catch (error) {
     console.error("error:", error);
   }
@@ -39,35 +39,15 @@ function displayPokemons(pokemon) {
               <div class="desc">${pokemon.name}</div>
             </div>
          `;
+    card.addEventListener("click", () => {
+      showModal();
+    });
     main.appendChild(card);
   });
 }
 
-function modal(pokemon) {
-  main.innerHTML = "";
-
-  pokemon.forEach((pokemon) => {
-    const pokemonID = pokemon.url.split("/")[6];
-    const card = document.createElement("div");
-    card.className = "responsive";
-    card.innerHTML = `
-            <div class="card">
-              <button id="myBtn">
-                <img
-                  src="https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/dream-world/${pokemonID}.svg"
-                  alt="Cinque Terre"
-                />
-              </button>
-              <div class="desc">${pokemon.name}</div>
-            </div>
-         `;
-
-    main.appendChild(card);
-  });
-}
-
-showModal();
-closeModal();
+//  for test
+// showModal();
 
 function showModal() {
   const modal = document.getElementById("myModal");
@@ -87,7 +67,8 @@ function closeModal() {
 
 var span = document.getElementsByClassName("close")[0];
 span.onclick = closeModal;
-// When the user clicks anywhere outside of the modal, close it
+
+const modal = document.getElementById("myModal");
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
