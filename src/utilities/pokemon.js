a619a3969de2115bc7eab0163f2e13571b8c4472
David Antonio <happywybie@gmail.com>
2024-07-21 20:11:43 -0600

    refactor(card): define structure for favorites

--- src/utilities/pokemon.js
+++ src/utilities/pokemon.js
@@ -1,9 +1,8 @@
-const max_pokemon = 1;
+const max_pokemon = 20;
 const listDisplay = document.querySelector(".list-display");
 const main = document.querySelector(".main");
 
 let pokemons = [];
-
 
 async function fetchData() {
   try {
@@ -14,7 +13,7 @@ async function fetchData() {
     const data = await response.json();
     pokemons = data.results;
     console.log(pokemons);
-    // displayPokemons(pokemons);
+    displayPokemons(pokemons);
   } catch (error) {
     console.error("error:", error);
   }
@@ -31,6 +30,32 @@ function displayPokemons(pokemon) {
     card.className = "responsive";
     card.innerHTML = `
             <div class="card">
+             <!-- favorities -->
+              <input
+                type="checkbox"
+                id="favorite"
+                class="checkbox-heart"
+                name="favorite-checkbox"
+                value="favorite-button"
+              />
+              <label for="favorite" class="containerHeart">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-heart"
+                >
+                  <path
+                    d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
+                  ></path>
+                </svg>
+              </label>
               <button id="myBtn" class="modal-btn">
                 <img
                   src="https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/dream-world/${pokemonID}.svg"
@@ -70,5 +95,3 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
-
-
