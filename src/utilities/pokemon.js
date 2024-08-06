f50358331e3b63ccba2e7f6cd17631ede4a00fb5
David Antonio <happywybie@gmail.com>
2024-08-05 19:33:01 -0600

    ci(head): add summary style and config to show and hidden

--- src/utilities/pokemon.js
+++ src/utilities/pokemon.js
@@ -474,3 +474,23 @@ function handleSearch() {
     notFoundMessage.style.display = "none";
   }
 }
+
+
+// Fetch all the details element.
+const details = document.querySelectorAll("details");
+
+// Add the onclick listeners.
+details.forEach((detail) => {
+  detail.addEventListener("toggle", () => {
+    if (detail.open) setTargetDetail(detail);
+  });
+});
+
+// Close all the details that are not targetDetail.
+function setTargetDetail(targetDetail) {
+  details.forEach((detail) => {
+    if (detail !== targetDetail) {
+      detail.open = false;
+    }
+  });
+}
